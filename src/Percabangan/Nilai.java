2ba2eca62921fb149849f80af316b11a71d3fe33
renaldiilhami24 <renaldiilhami24@gmail.com>
2020-04-10 14:36:17 +0800

    Dosen & Mahasiswa Teladan

---
+++ src/Percabangan/Nilai.java
@@ -0,0 +1,31 @@
+package Percabangan;
+
+public class Nilai {
+    public static void main(String[] args){
+        char nilai= 'D';
+        switch(nilai){
+            case 'A':
+            System.out.println(" Mhs: \"Tengkyu Pak\"");
+            System.out.println(" Dosen: \"Selamat ya !!\"");
+            break;
+            case 'B':
+                System.out.println(" Mhs: \"Kenapa saya dapat A pak ?\"");
+                System.out.println(" Dosen: \"Good Job !!\"");
+                break;
+            case 'C':
+                System.out.println(" Mhs: \"Saya turun full pak tugas ngumpul juga\"");
+                System.out.println(" Dosen: \"Tapi bisalah menjawab ujian\"");
+                System.out.println(" Mhs: \"Isyaallah pak wkwkwk\"");
+                break;
+            default:
+                System.out.println(" Mhs: \"Saya turun full pak tugas ngumpul juga\"");
+                System.out.println(" Dosen: \"iya kah\"");
+                System.out.println(" Dosen: \"Melihati berkas\"");
+                System.out.println(" Dosen: \"Bah dusta nih wkwk\"");
+                System.out.println(" Mhsa: \"Auto bukah wkwk.... \"");
+                break;
+
+        }
+    }
+
+}
