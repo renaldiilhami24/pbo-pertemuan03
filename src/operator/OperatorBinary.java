9e759087878d83bfb1c0fa00131e1304df27f468
renaldiilhami24 <renaldiilhami24@gmail.com>
2020-04-10 13:35:59 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai ke dalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh progam
+
+    String nama = "Muhammad Renaldi Ilhami";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    
+}
